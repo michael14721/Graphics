0b7d6aea2a1bac2c6941beb049162e20c5b765f8
Kenny <201409301@post.au.dk>
2018-02-19 22:53:25 +0100

    GRAVITY

--- GraphicsNetFramework/GraphicsNetFramework/Objects/Player.cs
+++ GraphicsNetFramework/GraphicsNetFramework/Objects/Player.cs
@@ -14,7 +14,11 @@ namespace Graphics.Objects
 		private readonly Stopwatch _stopwatchDown;
 		private readonly Stopwatch _stopwatchLeft;
 		private readonly Stopwatch _stopwatchRight;
+		private readonly Stopwatch _stopwatchGravity;
+		private readonly TimeSpan _gravityTimeSpan = TimeSpan.FromMilliseconds(200);
 		private readonly TimeSpan _movementSpan = TimeSpan.FromMilliseconds(80);
+		private readonly double _gravity = 10;
+		private double _acceleration = 0;
 
 		public Player(InputHandler inputHandler, CollisionMap collisionMap, Cave cave, int width)
 		{
@@ -23,11 +27,13 @@ namespace Graphics.Objects
 			_cave = cave;
 			_width = width;
 
+			_stopwatchGravity = new Stopwatch();
 			_stopwatchRight = new Stopwatch();
 			_stopwatchLeft = new Stopwatch();
 			_stopwatchUp = new Stopwatch();
 			_stopwatchDown = new Stopwatch();
 
+			_stopwatchGravity.Start();
 			_stopwatchRight.Start();
 			_stopwatchLeft.Start();
 			_stopwatchUp.Start();
@@ -44,12 +50,52 @@ namespace Graphics.Objects
 
 			RegisterInputHandlers();
 		}
-		
+
+		public override void Step()
+		{
+			base.Step();
+			if (_stopwatchGravity.Elapsed < _gravityTimeSpan)
+			{
+				return;
+			}
+			else
+			{
+				_acceleration += _gravity;
+				switch (Math.Sign(Math.Floor(_acceleration)))
+				{
+					case -1:
+						if (!_collisionMap.IsFree(X, Y - 1))
+						{
+							_acceleration = 0;
+						}
+						else
+						{
+							Y -= 1;
+						}
+						break;
+					case 1:
+						if (!_collisionMap.IsFree(X, Y + 1))
+						{
+							_acceleration = 0;
+						}
+						else
+						{
+							Y += 1;
+						}
+						break;
+					default:
+						break;
+				}
+			}
+
+			_stopwatchGravity.Restart();
+		}
+
 		private void RegisterInputHandlers()
 		{
 			_inputHandler.AddHandler(Key.W, () =>
 			{
-				if (_collisionMap.IsFree(X, Y - 1))
+				if (!_collisionMap.IsFree(X, Y + 1))
 				{
 					if (_stopwatchUp.Elapsed < _movementSpan)
 					{
@@ -57,7 +103,7 @@ namespace Graphics.Objects
 					}
 					else
 					{
-						Y -= 1;
+						_acceleration = -50;
 						_stopwatchUp.Restart();
 					}
 				}
